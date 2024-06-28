28521a15415ac48c7d968bea716f01edb7c66d64
AlinaGryshchenko <alinahryshchenkoios@gmail.com>
2024-06-28 11:57:38 +0100

    first commit

---
+++ TestCards/FlascardCell.swift
@@ -0,0 +1,17 @@
+//
+//  FlascardCell.swift
+//  TestCards
+//
+//  Created by Алина Лошакова on 26.06.2024.
+//
+
+import UIKit
+
+class FlascardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TestCards/WordsModel.swift
@@ -0,0 +1,8 @@
+//
+//  WordsModel.swift
+//  TestCards
+//
+//  Created by Алина Лошакова on 26.06.2024.
+//
+
+import Foundation
